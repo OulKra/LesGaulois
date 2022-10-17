abbd4f89860f498ac345f98991ee534b7cb66639
gabernet louis <gbl4455a@ens.fsi>
2022-10-17 17:48:22 +0200

    lol

--- src/personnages/Gaulois.java
+++ src/personnages/Gaulois.java
@@ -2,8 +2,12 @@ package personnages;
 
 public class Gaulois {
 	private String nom;
-	private int force;
+	//private int force;
 	private int effetPotion = 1;
+
+	
+	private int force, nb_trophees;
+	private Equipement trophees[] = new Equipement[100];
 	
 	public Gaulois(String nom, int force) {
 		super();
@@ -23,15 +27,34 @@ public class Gaulois {
 		System.out.println(prendreParole() + "«" + texte + "»");
 	}
 	
-	private String prendreParole() {
+	/*private String prendreParole() {
 		return "Le gaulois " + nom + " : ";
-	}
+	}*/
 	
-	public void frapper(Romain romain) {
+	private String prendreParole() {
+		String texte = "Le gaulois " + nom + " : ";
+		return texte;
+		}
+
+	
+	/*public void frapper(Romain romain) {
 		System.out.println(nom + " envoie un grand coup dans la machoire de " + romain.getNom());
 		romain.recevoirCoup(force / 3);
+	}*/
+
+	public void frapper(Romain romain) 
+	{
+		System.out.println(nom + " envoie un grand coup dans la	mâchoire de " + romain.getNom());
+		Equipement trophees[] = romain.recevoirCoup((force / 3) *
+		effetPotion);
+		for (int i = 0; trophees != null && i < trophees.length; i++, nb_trophees++) 
+		{
+			this.trophees[nb_trophees] = trophees[i];
+		}
+		return;
 	}
 
+	
 	void boirePotion(int forcePotion)
 	{
 		effetPotion = forcePotion;
--- src/personnages/Romain.java
+++ src/personnages/Romain.java
@@ -17,7 +17,7 @@ public class Romain {
  	private String prendreParole() {
 		return "Le romain " + nom + " : ";
 	}
-	public void recevoirCoup(int forceCoup) {
+	/*public void recevoirCoup(int forceCoup) {
 		assert force > 0: "La force doit être positive";
 		int force_tmp = force;
 		force -= forceCoup;
@@ -27,7 +27,87 @@ public class Romain {
 			parler("J'abandonne...");
 		}
 		assert force < force_tmp: "La force n'a pas diminuée";
-	}
+	}*/
+ 	
+ 	public Equipement[] recevoirCoup(int forceCoup) {
+ 		Equipement[] equipementEjecte = null;
+ 		// précondition
+ 		assert force > 0;
+ 		int oldForce = force;
+ 		forceCoup = CalculResistanceEquipement(forceCoup);
+ 		force -= forceCoup;
+ 		// if (force > 0) {
+ 		// parler("Aïe");
+ 		// } else {
+ 		// equipementEjecte = ejecterEquipement();
+ 		// parler("J'abandonne...");
+ 		// }
+ 		switch (force) {
+	 		case 0:
+	 		parler("Aïe");
+	 		default:
+			equipementEjecte = ejecterEquipement();
+			parler("J'abandonne...");
+			break;
+		}
+		// post condition la force à diminuer
+		assert force < oldForce;
+		return equipementEjecte;
+		}
+
+ 	private int CalculResistanceEquipement(int forceCoup) 
+ 	{
+ 		String texte = "Ma force est de " + this.force + ", et la force du coup est de " + forceCoup;
+ 		int resistanceEquipement = 0;
+ 		if (!(nbrEquipement == 0)) 
+ 		{
+	 		texte += "\n Mais heureusement, grace à mon équipement sa force est diminué de ";
+	 		for (int i = 0; i < nbrEquipement;) {
+	 		if ((equipement[i] != null &&
+	 		equipement[i].equals(Equipement.BOUCLIER)) == true) {
+	 		resistanceEquipement += 8;
+	 		} else {
+	 		System.out.println("Equipement casque");
+	 		resistanceEquipement += 5;
+	 		}
+	 		i++;
+	 		}
+	 		texte =+ resistanceEquipement + "!";
+ 		}
+ 		parler(texte);
+ 		forceCoup -= resistanceEquipement;
+ 		return forceCoup;
+ 		}
+ 		private Equipement[] ejecterEquipement() 
+ 		{
+	 		Equipement[] equipementEjecte = new Equipement[nbEquipement];
+	 		System.out.println("L'équipement de " + nom.toString() + "
+	 		s'envole sous la force du coup.");
+	 		//TODO
+	 		int nbEquipementEjecte = 0;
+	 		for (int i = 0; i < nbEquipement; i++) 
+	 		{
+	 			if (equipements[i] == null) 
+	 			{
+	 				continue;
+	 			} 
+	 			else 
+	 			{
+	 				equipementEjecte[nbEquipementEjecte] =
+	 				equipements[i];
+	 				nbEquipementEjecte++;
+	 				equipements[i] = null;
+	 			}
+	 		}
+	 		return equipementEjecte;
+ 		}
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
 	public String getNom() {
 		// TODO Auto-generated method stub
 		return nom;
